88ac07c91fc1a9640609fca06c33a1b8f6ca7d4e
Ryan Paul C. Rodanilla <rodanillaryan@gmail.com>
2024-09-24 16:14:08 +0800

    User Profile Created

---
+++ qrReaderPy/userprofile/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
+
+#Unregistering Models
+# admin.site.unregister(Group)
+
+#Mix Profile Info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+#Extending User Modal
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields =["username", "first_name", "last_name", "email", "is_staff", "is_active"]
+    inlines = [ProfileInline]
+
+#Unregistering Initial User
+admin.site.unregister(User)
+
+#Reregister User and Profile
+admin.site.register(User, UserAdmin)
+#admin.site.register(Profile)
---
+++ qrReaderPy/userprofile/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    MALE = 'M'
+    FEMALE = 'F'
+
+    GENDER_CHOICES = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+    ]
+
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE)
+    address = models.TextField(blank=False, null=False)
+
+    contact_number = models.CharField(max_length=25)
+    age = models.IntegerField(null=False, blank=False)
+    gender = models.CharField(
+        max_length=10, choices=GENDER_CHOICES, default=MALE)
+    profile_photo = models.ImageField(
+        upload_to='images/profiles/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.user.username
+    
+# #Create profile when new user signed up
+# def create_profile(sender, instance, created, **kwargs):
+#     if created:
+#         user_profile = Profile(user=instance)
+#         user_profile.save()
+
+# post_save.connect(create_profile, sender=User)
