cb2a3c4e3b64b395b6edaaf821a782f9b081eaa4
Ryan Paul C. Rodanilla <rodanillaryan@gmail.com>
2024-09-26 14:04:02 +0800

    User Profile Implementation

--- qrReaderPy/userprofile/models.py
+++ qrReaderPy/userprofile/models.py
@@ -27,10 +27,3 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
     
-# #Create profile when new user signed up
-# def create_profile(sender, instance, created, **kwargs):
-#     if created:
-#         user_profile = Profile(user=instance)
-#         user_profile.save()
-
-# post_save.connect(create_profile, sender=User)
