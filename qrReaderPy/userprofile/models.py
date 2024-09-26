cedb20ee6a5bba459edc133e974efae493238023
Ryan Paul C. Rodanilla <rodanillaryan@gmail.com>
2024-09-26 14:19:11 +0800

    Removed Duplicates

--- qrReaderPy/userprofile/models.py
+++ qrReaderPy/userprofile/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
     MALE = 'M'
     FEMALE = 'F'
 
@@ -25,12 +22,4 @@ class Profile(models.Model):
         upload_to='images/profiles/', blank=True, null=True)
     
     def __str__(self):
-        return self.user.username
-    
-# #Create profile when new user signed up
-# def create_profile(sender, instance, created, **kwargs):
-#     if created:
-#         user_profile = Profile(user=instance)
-#         user_profile.save()
-
-# post_save.connect(create_profile, sender=User)
+        return self.user.username 
